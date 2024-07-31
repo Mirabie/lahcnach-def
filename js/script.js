1995bb1af6369641705e1a7567264112ddf8920a
Mirabie <mirmdshabie@gmail.com>
2024-08-01 01:12:23 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const button = document.getElementById('clickButton');
+    button.addEventListener('click', () => {
+        alert('Button Clicked!');
+    });
+});
